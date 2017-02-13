9e0d95d4e6e2c5a95fc22cb96bf6d826cd6a0d8c
dlkfjl lsadjfol <dlkfjl lsadjfol>
2017-02-12 17:56:43 -1000

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cunninghamApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cunninghamApp
+ */
+angular.module('cunninghamApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cunninghamApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cunninghamApp
+ */
+angular.module('cunninghamApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
